094a2cf762b03ecde863e284dc623bfd596338ce
clydewbrown <clydewbrown@gmail.com>
2013-08-13 10:56:07 -0400

    Source file for Assignment #5 - Insurance Premium

---
+++ src/InsurancePremium.java
@@ -0,0 +1,23 @@
+// -------INSTRUCTIONS-------
+// 1. Complete the exercise as indicated on the handout for assignment #5
+// 2. Then delete these comments
+// 3. Finally, commit and push the project containing this file to GitHub
+
+// -------RUBRIC-------
+// Functionality								/6
+// Methods adhere to program requirements	/5
+// Value returned from calculatePremium()	/3
+// Coding conventions & comments				/6
+
+
+public class InsurancePremium {
+
+	public static void main(String[] args) {
+		//Insert body of main() method here
+	}
+	
+	public static int calculatePremium(/*Insert parameters here*/) {
+		//Insert body of calculatePremium() method here
+	}
+
+}
